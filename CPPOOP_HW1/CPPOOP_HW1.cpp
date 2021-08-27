7c2f2f41a47d47af841312dc3be4e21123d513b5
ivanv <ivanv@DESKTOP-MMG7R03>
2021-08-27 16:22:58 +0300

    Task 2 complete

--- CPPOOP_HW1/CPPOOP_HW1.cpp
+++ CPPOOP_HW1/CPPOOP_HW1.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 
+
+//Task 1 ==================================================
 class Power 
 {
 public:
@@ -26,6 +28,37 @@ private:
     double secondNumber;
 };
 
+//Task 2 ====================================================
+
+class RGBA
+{
+
+public:
+    RGBA()
+    {
+        m_red = 0;
+        m_green = 0;
+        m_blue = 0;
+        m_alpha = 255;
+    }
+    RGBA(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha)
+    {
+        m_red = red;
+        m_green = green;
+        m_blue = blue;
+        m_alpha = alpha;
+    }
+
+    void print() 
+    {
+        std::cout << "\n\nRed: " << static_cast<int>(m_red) << "\nGreen: " << static_cast<int>(m_green) << "\nBlue: " << static_cast<int>(m_blue) << "\nAlpha: " << static_cast<int>(m_alpha);
+    }
+private:
+    std::uint8_t m_red, m_green, m_blue, m_alpha;
+};
+
+
+
 int main()
 {
     //Task1
@@ -34,6 +67,17 @@ int main()
     std::cout << num->calculate();
     delete num;
 
+
+    //Task2
+    RGBA* color = new RGBA(5,5,5,100);
+    color->print();
+    delete color;
+
+    RGBA* color2 = new RGBA();
+    color2->print();
+    delete color2;
+
+    
     
 }
 
